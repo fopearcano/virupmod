d052867c5383276e1339c9e7b798e1d2a6d96b5f
Florian Cabot <florian.cabot@epfl.ch>
2023-02-06 18:16:58 +0100

    Fix async loading

--- virup/src/methods/OctreeLOD.cpp
+++ virup/src/methods/OctreeLOD.cpp
@@ -303,12 +303,12 @@ void OctreeLOD::update(Camera const& camera, QMatrix4x4 const& globalModel,
 		/*if(usedMem() < memLimit())
 		{*/
 		// SYNC
-		readOwnData(*file);
-		ramToVideo();
+		// readOwnData(*file);
+		// ramToVideo();
 		// ASYNC
-		// state = AsyncReader::State::WAIT;
-		// AsyncReader::load(*this);
-		// doRender = false;
+		state = AsyncReader::State::WAIT;
+		AsyncReader::load(*this);
+		doRender = false;
 		/*}
 		else
 		{
