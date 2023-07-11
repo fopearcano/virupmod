f6ba945579a77084f69548d8cd610b8fc5f75df1
Florian Cabot <florian.cabot@epfl.ch>
2023-07-11 19:08:11 +0200

    Update HydrogenVR to 2092509b85bc90c70b6dee6c705f16c337817c86

--- test/main.cpp
+++ test/main.cpp
@@ -5,10 +5,9 @@
 int main(int argc, char** argv)
 {
 	unsigned int status = 0;
-	auto ASSERT_TEST    = [&status, argc, argv](QObject* obj)
-	{
-		status |= static_cast<unsigned int>(QTest::qExec(obj, argc, argv));
-		delete obj;
+	auto ASSERT_TEST    = [&status, argc, argv](std::unique_ptr<QObject> obj) {
+        status
+            |= static_cast<unsigned int>(QTest::qExec(obj.get(), argc, argv));
 	};
 
 	test_main(ASSERT_TEST);
