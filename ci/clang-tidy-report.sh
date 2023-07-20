6c166020c633b1a703ed1ab757fe5a655c399407
Florian Cabot <florian.cabot@epfl.ch>
2023-07-20 16:49:33 +0200

    Update HydrogenVR to a852b3401e9b28bb205803969113a2df7182b4a7

--- ci/clang-tidy-report.sh
+++ ci/clang-tidy-report.sh
@@ -30,6 +30,9 @@ do
 	i=$((i + 1))
 done
 
+checksums_file="passed_tidy.txt"
+touch $checksums_file
+
 echo "-=-=-=- LAUNCHING CLANG-TIDY -=-=-=-"
 # run all clang-tidies in parallel
 max_parallel=$(nproc)
@@ -51,8 +54,17 @@ do
 	j=$(($j + 1))
 	out=/tmp/clang_tidy_${file_name}.out
 
-	echo $f
-	$1 --use-color $f ${@:${i}} 2> /dev/null > $out &
+	# Calculate the checksum of the current file
+	checksum=$(sha256sum "$f" | cut -d' ' -f1)
+
+	# If the checksum is not in the checksums_file, execute the long-running command
+	if ! grep -q "$checksum" "$checksums_file"; then
+		# Run the long command
+		echo $f
+		$1 --use-color $f ${@:${i}} 2> /dev/null > $out &
+	else
+		touch $out
+	fi
 done
 
 # wait for them to finish
@@ -68,11 +80,18 @@ do
 		file_name="$(basename $(dirname $f))_$(basename $f)"
 	fi
 	out=/tmp/clang_tidy_${file_name}.out
+
 	result=$(cat $out)
 
 	if [[ "$result" != "" ]]; then
 		cat $out
 		error=1
+	else
+		# Calculate the checksum of the current file
+		checksum=$(sha256sum "$f" | cut -d' ' -f1)
+
+		# If the command succeeded, store the checksum
+		echo "$checksum" >> "$checksums_file"
 	fi
 	rm $out
 done
