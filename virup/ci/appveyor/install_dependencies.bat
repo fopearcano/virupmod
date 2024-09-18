be84ec5ecbf1806df1b54b96de1093bd035642cc
Florian Cabot <florian.cabot@epfl.ch>
2024-09-18 14:00:04 +0200

    Upgrade liboctree to 1.17.3 for AppVeyor

--- virup/ci/appveyor/install_dependencies.bat
+++ virup/ci/appveyor/install_dependencies.bat
@@ -3,7 +3,7 @@
 ::############################################################################
 mkdir octree
 cd octree
-set URL="https://gitlab.com/api/v4/projects/9621748/packages/generic/releases/1.17.2/liboctree-1.17.2-windows-%BUILD_TYPE%.zip"
+set URL="https://gitlab.com/api/v4/projects/9621748/packages/generic/releases/1.17.3/liboctree-1.17.3-windows-%BUILD_TYPE%.zip"
 appveyor DownloadFile %URL% -FileName octree.zip
 7z x octree.zip > nul
 move liboctree* liboctree
